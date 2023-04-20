0afc5f45ace3107dbc81de29c98f1ff26e22fbab
丹佑太 <yutan@tanyuutanoMacBook-Air.local>
2023-04-20 09:48:43 +0900

    initial commit

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEDAL"
+	EOF     = "EOF"
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	// ()	丸括弧	パーレンシス(parenthesis)
+	// []	角括弧	ブラケット(bracket)
+	// {}	波括弧	ブレース(brace)
+	LRAREN = "("
+	RRAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
